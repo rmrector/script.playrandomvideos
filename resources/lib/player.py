220d7f0b4ef185f6cfd34dee0608be492a3129ed
Ryan Rector <rmrector@gmail.com>
2016-10-15 22:54:19 -0600

    Fix up player
    Fix pause causing the script to stop adding new items
    Load one video at a time rather than chunks

--- resources/lib/player.py
+++ resources/lib/player.py
@@ -4,18 +4,17 @@ from listitembuilder import build_video_listitem
 import quickjson
 from pykodi import log
 
-EXTEND_WHEN = 3 # Add more items when there are only X left
-EXTEND_BY = 5
-REMOVE_OLDER_THAN = 4 # Keep only X previously played
+LOAD_NEW = 3
+KEEP_PREVIOUS = 4
 
 def get_player(source):
-    result = ChunkPlayer()
+    result = RollingPlaylistPlayer()
     result.source = source
     return result
 
-class ChunkPlayer(xbmc.Player):
+class RollingPlaylistPlayer(xbmc.Player):
     def __init__(self):
-        super(ChunkPlayer, self).__init__()
+        super(RollingPlaylistPlayer, self).__init__()
         self.playlist = xbmc.PlayList(xbmc.PLAYLIST_VIDEO)
         self.playlist.clear()
         self._source = None
@@ -34,34 +33,34 @@ class ChunkPlayer(xbmc.Player):
     def run(self):
         if self.extend_playlist():
             self.play(self.playlist)
-        loopbuffer = 0
-        while self.isPlaying() or loopbuffer < 2:
+            if LOAD_NEW - 1 > 0:
+                self.extend_playlist(LOAD_NEW - 1)
+        while xbmc.getCondVisibility('Player.HasMedia'):
             if self.source_exhausted:
                 break
             xbmc.sleep(2000)
-            loopbuffer = 0 if self.isPlaying() else (loopbuffer + 1)
-        log("I'm done", xbmc.LOGNOTICE)
+
+        log("I'm done", xbmc.LOGINFO)
 
     def add_to_playlist(self, item):
         self.playlist.add(item.get('file'), build_video_listitem(item))
 
-    def extend_playlist(self):
+    def extend_playlist(self, count=1):
         if self.source_exhausted:
             return False
-        count = 0
-        for item in self._source:
-            self.add_to_playlist(item)
-            count += 1
-            if count >= EXTEND_BY:
+        addedcount = 0
+        for _ in range(0, count):
+            try:
+                self.add_to_playlist(next(self._source))
+                addedcount += 1
+            except StopIteration:
+                self.source_exhausted = True
+                log("Source has been exhausted", xbmc.LOGINFO)
                 break
-        self.source_exhausted = count < EXTEND_BY
-        if (self.source_exhausted):
-            log("Source has been exhausted", xbmc.LOGNOTICE)
 
-        return True if count else False
+        return True if addedcount else False
 
     def onPlayBackStarted(self):
-        if self.playlist.getposition() >= len(self.playlist) - 1 - EXTEND_WHEN:
-            self.extend_playlist()
-        if self.playlist.getposition() > REMOVE_OLDER_THAN:
+        self.extend_playlist()
+        if self.playlist.getposition() > KEEP_PREVIOUS:
             quickjson.remove_from_playlist(0)
