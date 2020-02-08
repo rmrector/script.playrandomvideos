05f18c851f70c3d085187d81f159e86a4505bd7d
Ryan Rector <rmrector@gmail.com>
2020-02-08 14:40:48 -0700

    Remove Krypton backward-compatibility

--- python/context.py
+++ python/context.py
@@ -4,9 +4,6 @@ import xbmc
 from lib import playrandom
 from lib.pykodi import get_pathinfo
 
-# DEPRECATED: StringCompare and SubString in addon.xml is deprecated in Krypton, gone in Leia,
-#  but both options resolve to False when unrecognized so the result is the same for all versions
-
 if __name__ == '__main__':
     path = sys.listitem.getfilename()
     label = sys.listitem.getLabel()
